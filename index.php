140281d66aba6bddbfc875924f74b033d528740a
Alati11 <peppealati@gmail.com>
2023-11-28 16:48:22 +0100

    print my production

--- index.php
+++ index.php
@@ -9,14 +9,37 @@ class Production
     function __construct($_title, $_language, $_rating)   
     {
       $this->title = $_title;
-      $this->languade = $_languade;
+      $this->language = $_language;
       $this->rating = $_rating;
+    }
+
+    public function myfilms() {
+        echo "Film preferito:";
     }
 }
 
-$film1 = new Product('Seven', 'Inglese', '5 stelle');
-$film2 = new Product('Inception', 'Inglese', '4 stelle');
-$film3 = new Product('C era una volta in America', 'Italiano', '5 stelle');
 
 
-var_dump($film1);
+$film1 = new Production('Seven', 'Inglese', '5');
+$film2 = new Production('Inception', 'Inglese', '4');
+$film3 = new Production('C era una volta in America', 'Italiano', '5');
+
+
+
+$films = [
+    $film1,
+    $film2,
+    $film3,
+];
+
+var_dump($films);
+
+
+foreach ($films as $production) {
+?>
+    <h4><?php $production->myfilms() ?></h4>    
+     <div>
+        <p><?= $production->title. ' ' . $production->language. ' '. $production->rating ?></p>    
+    </div>
+<?php
+}
