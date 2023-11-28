0dd8a461b509f4d5cefd4bb5818bd7cf2f671200
Alati11 <peppealati@gmail.com>
2023-11-28 17:40:09 +0100

    add function setRating

--- index.php
+++ index.php
@@ -10,22 +10,34 @@ class Production
     {
       $this->title = $_title;
       $this->language = $_language;
-      $this->rating = $_rating;
+      $this->setRating($_rating);
     }
 
     public function myfilms() {
         echo "Film preferito:";
     }
+
+    public function setRating($rating)
+    {
+      if (is_numeric($rating) && $rating >= 3) {
+        $this->rating = intval($rating);    
+      } else {
+        // var_dump('Il voto è troppo basso...');
+        echo 'Il votro è troppo basso...';
+      }
+    }
+
 }
 
-$film1 = new Production('Seven', 'Inglese', '5');
-$film2 = new Production('Inception', 'Inglese', '4');
-$film3 = new Production('C era una volta in America', 'Italiano', '5');
-$film4 = new Production('Fight Club', 'Italiano', '5');
-$film5 = new Production('Il buono, il brutto e i cattivo', 'Italiano', '5');
-$film6 = new Production('Matrix', 'Inglese', '5');
-$film7 = new Production('L uomo in più', 'Italiano', '5');
-$film8 = new Production('Eyes wide shute', 'Inglese', '5');
+$film1 = new Production('Seven - ', 'Inglese', '5');
+$film2 = new Production('Inception - ', 'Inglese', '4');
+$film3 = new Production('C era una volta in America -', 'Italiano', '5');
+$film4 = new Production('Fight Club - ', 'Italiano', '5');
+$film5 = new Production('Il buono, il brutto e il cattivo - ', 'Italiano', '5');
+$film6 = new Production('Matrix - ', 'Inglese', '5');
+$film7 = new Production('L uomo in più - ', 'Italiano', '5');
+$film8 = new Production('Eyes wide shute - ', 'Inglese', '5');
+$film9 = new Production('Eyes wide shute - ', 'Inglese', '2');
 
 
 $films = [
@@ -35,14 +47,13 @@ $films = [
     $film5,
     $film6,
     $film7,
-    $film8
+    $film8,
+    $film9,
 ];
-
 // var_dump($films);
-
-
-
 ?>
+
+<h1>I miei film preferiti</h1>
 <?php foreach($films as $production) { ?>
 	<li><?php echo $production->title. ' ' . $production->language. ' '. $production->rating; ?>
 <?php } ?>
