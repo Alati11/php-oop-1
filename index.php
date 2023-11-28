4f9beeefe26f58e257ea11b38f16a8a3757bf351
Alati11 <peppealati@gmail.com>
2023-11-28 17:18:28 +0100

    print new film

--- index.php
+++ index.php
@@ -18,28 +18,31 @@ class Production
     }
 }
 
-
-
 $film1 = new Production('Seven', 'Inglese', '5');
 $film2 = new Production('Inception', 'Inglese', '4');
 $film3 = new Production('C era una volta in America', 'Italiano', '5');
-
+$film4 = new Production('Fight Club', 'Italiano', '5');
+$film5 = new Production('Il buono, il brutto e i cattivo', 'Italiano', '5');
+$film6 = new Production('Matrix', 'Inglese', '5');
+$film7 = new Production('L uomo in più', 'Italiano', '5');
+$film8 = new Production('Eyes wide shute', 'Inglese', '5');
 
 
 $films = [
     $film1,
     $film2,
     $film3,
+    $film5,
+    $film6,
+    $film7,
+    $film8
 ];
 
-var_dump($films);
+// var_dump($films);
 
 
-foreach ($films as $production) {
+
 ?>
-    <h4><?php $production->myfilms() ?></h4>    
-     <div>
-        <p><?= $production->title. ' ' . $production->language. ' '. $production->rating ?></p>    
-    </div>
-<?php
-}
+<?php foreach($films as $production) { ?>
+	<li><?php echo $production->title. ' ' . $production->language. ' '. $production->rating; ?>
+<?php } ?>
